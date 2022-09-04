b774afa1bc71e9aa31bde57b3f84c0634ee664a9
rvanderlely <101234963+rvanderlely@users.noreply.github.com>
2022-09-03 19:11:44 -0500

    adding in print functions bc proffesor wants them?

--- print_8_bits.cpp
+++ print_8_bits.cpp
@@ -19,17 +19,29 @@ using std::endl;
 *************************************************************************************/
 string generate_binary_string(int decimal);
 string convert_bin_hex(string binary);
+void print_binary(string binary);
+void print_hex(string hex);
 /************************************************************************************
  *                                  MAIN
 *************************************************************************************/
 int main()
 {
+    cout <<"-------------------------------------------------"<<endl;
     cout<<"Decimal:\tBinary:\t\tHexadecimal:\t"<<endl;
-
+    cout <<"-------------------------------------------------"<<endl;
     for (int i = 0;i<256;i++)
-    {
-    cout << i << "\t\t" << generate_binary_string(i) << "\tFF:" << endl;
-    }
+        {
+        //Do conversions
+        string binary = generate_binary_string(i);
+        string hex = convert_bin_hex(binary);
+
+        //Print Results. This one was single line before I was required to add print functions?
+        cout << i << ":\t\t";
+        print_binary(binary);
+        cout<< ":\t" << ":";
+        print_hex(hex);
+        cout<< endl;
+        }
     return 0;
 }
 /************************************************************************************
@@ -61,5 +73,73 @@ Output: Returns a string containing the hexadecimal number of the binary passed 
 *************************************************************************************/  
 string convert_bin_hex(string binary)
 {
+    string hex ="";
+    string bin1 ="";
+    string bin2 ="";
+    for (int i = 0; i <= 3; i++)
+    {
+        bin1 = bin1 + binary[i];
+        bin2 = bin2 + binary[i+4];
+    }
 
+
+   if   (bin1 == "0000"){hex = "0";}
+   else if (bin1 == "0001"){hex = "1";}
+   else if (bin1 == "0010"){hex = "2";}
+   else if (bin1 == "0011"){hex = "3";}
+   else if (bin1 == "0100"){hex = "4";}
+   else if (bin1 == "0101"){hex = "5";}
+   else if (bin1 == "0110"){hex = "6";}
+   else if (bin1 == "0111"){hex = "7";}
+   else if (bin1 == "1000"){hex = "8";}
+   else if (bin1 == "1001"){hex = "9";}
+   else if (bin1 == "1010"){hex = "A";}
+   else if (bin1 == "1011"){hex = "B";}
+   else if (bin1 == "1100"){hex = "C";}
+   else if (bin1 == "1101"){hex = "D";}
+   else if (bin1 == "1110"){hex = "E";}
+   else if (bin1 == "1111"){hex = "F";}
+
+   if (bin2 == "0000"){hex = hex + "0";}
+   else if (bin2 == "0001"){hex = hex + "1";}
+   else if (bin2 == "0010"){hex = hex + "2";}
+   else if (bin2 == "0011"){hex = hex + "3";}
+   else if (bin2 == "0100"){hex = hex + "4";}
+   else if (bin2 == "0101"){hex = hex + "5";}
+   else if (bin2 == "0110"){hex = hex + "6";}
+   else if (bin2 == "0111"){hex = hex + "7";}
+   else if (bin2 == "1000"){hex = hex + "8";}
+   else if (bin2 == "1001"){hex = hex + "9";}
+   else if (bin2 == "1010"){hex = hex + "A";}
+   else if (bin2 == "1011"){hex = hex + "B";}
+   else if (bin2 == "1100"){hex = hex + "C";}
+   else if (bin2 == "1101"){hex = hex + "D";}
+   else if (bin2 == "1110"){hex = hex + "E";}
+   else if (bin2 == "1111"){hex = hex + "F";}
+
+return hex;
+
+}
+
+
+/************************************************************************************
+                    string print_binary(string binary)
+Input: The string that needs to be printed
+Output: Prints the binary number
+*************************************************************************************/
+void print_binary(string binary)
+{
+//Professor this entire function didn't seem like it needed to be here. However, i did it because it was required in the instructions. 
+cout<<binary;
+}
+
+/************************************************************************************
+                    string print_hex(string hex)
+Input: The string that needs to be printed
+Output: Prints the binary number
+*************************************************************************************/
+void print_hex(string hex)
+{
+//Professor this entire function didn't seem like it needed to be here. However, i did it because it was required in the instructions. 
+cout<<hex;
 }
